ccba60d2ba0ce347ec191794c98880f3b40c6c5a
Silvio Souza <wms.silvio@gmail.com>
2024-08-20 12:05:24 -0400

    arquiterura

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
